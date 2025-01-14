a985db2affdf5e773e04150ec1dfff3518077b5e
Aniket Sharma <aniket@noytindia.com>
2025-01-14 11:56:00 +0530

    let's check again after doing changes in redirect-uri on azure as well as on vercel

--- app.js
+++ app.js
@@ -27,7 +27,9 @@ import { EmailRoutes } from "./API/Routes/emailRoutes.js";
 app.use("/", EmailRoutes);
 
 app.get("/", (req, res) => {
-  res.send("hi this is working perfectly fine and port is", PORT);
+  res.send("hi this is working perfectly fine and port is");
+  console.log("port is", process.env.PORT);
+  console.log("port is", process.env.REDIRECT_URI);
 });
 // Verify subscription validation token
 app.post("/webhook", (req, res) => {
