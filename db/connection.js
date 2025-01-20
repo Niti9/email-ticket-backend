a71448c204f0c488dfbf932c60b73b84eac38fe3
Aniket Sharma <aniket@noytindia.com>
2025-01-20 16:42:17 +0530

    we are getting this error 'Legacy server listening... DB connection error: Invalid scheme, expected connection string to start with "mongodb://" or "mongodb+srv://" Node.js process exited with exit status: 1. The logs above can help with debugging the issue.' so we try to handle this

--- db/connection.js
+++ db/connection.js
@@ -1,28 +1,28 @@
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-// dotenv.config({ path: "./config.env" });
-
-// const DB = process.env.DATABASE;
-
-// mongoose
-//   .connect(DB)
-//   .then(() => {
-//     console.log("DB Connection successful");
-//   })
-//   .catch((err) => console.error("DB connection error:", err));
-
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config({ path: "./config.env" });
+
 const DB = process.env.DATABASE;
+
 mongoose
-  .connect(DB, {
-    serverSelectionTimeoutMS: 5000 // Adjust timeout for server selection
-  })
+  .connect(DB)
   .then(() => {
-    console.log(`MongoDB Connected:  with ${process.env.DATABASE}`);
+    console.log("DB Connection successful");
   })
-  .catch((err) => {
-    console.error(`DB connection error: ${err.message}`);
-    process.exit(1);
-  });
+  .catch((err) => console.error("DB connection error:", err));
+
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// dotenv.config({ path: "./config.env" });
+// const DB = process.env.DATABASE;
+// mongoose
+//   .connect(DB, {
+//     serverSelectionTimeoutMS: 5000 // Adjust timeout for server selection
+//   })
+//   .then(() => {
+//     console.log(`MongoDB Connected:  with ${process.env.DATABASE}`);
+//   })
+//   .catch((err) => {
+//     console.error(`DB connection error: ${err.message}`);
+//     process.exit(1);
+//   });
