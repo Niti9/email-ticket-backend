5f55fa7b2ee8e71f06a25aceca4e8b2c726a05a8
Nitin Negi <nitin.negi@noytindia.com>
2025-02-09 01:11:36 +0530

    pta nahi kuyn baar baar ye work karrha hai matlab ek bhi baar sahi se nahi

--- Database/models/EmailToken/ticketSchema.js
+++ Database/models/EmailToken/ticketSchema.js
@@ -33,7 +33,8 @@ const ticketSchema = new mongoose.Schema({
   conversationId: { type: String, required: true }, // Identify conversations
   comments: [
     {
-      commentId: { type: String, default: "", sparse: true }, // sparse allows multiple null values
+      commentId: { type: String, default: "" }, // 🔥 Prevent null values
+      // commentId: { type: String, default: "", sparse: true }, // sparse allows multiple null values
       // commentId: { type: String, unique: false, sparse: true }, // sparse allows multiple null values
       // commentId: { type: String, required: true, unique: true }, // Unique ID for each comment
       senderName: { type: String, required: true },
