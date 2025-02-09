9cdf2c8f7463e615f0807f5ae4ee5917bff38150
Nitin Negi <nitin.negi@noytindia.com>
2025-02-09 23:39:17 +0530

    its ok

--- Service/OutlookTicketService.js
+++ Service/OutlookTicketService.js
@@ -66,10 +66,6 @@ class OutlookTicketService {
     // }
     // console.log("alreadyExists are ", alreadyExists);
 
-    console.log(
-      "emailResponse conversationId is",
-      emailResponse.conversationId
-    );
     const create = await OutlookMailRepository.createNewTicket(
       tokenRecord,
       emailResponse
