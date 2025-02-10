5332b3ad11b9bacfe135393d0e674ef7647b66d0
Nitin Negi <nitin.negi@noytindia.com>
2025-02-10 11:02:06 +0530

    trying to not add duplicate comment

--- Service/OutlookCommentService.js
+++ Service/OutlookCommentService.js
@@ -9,34 +9,23 @@ class OutlookCommentService {
 
     // If this is a reply, add it as a comment
     if (existingTicket.conversationId === conversationId) {
-      // const isDuplicateComment = existingTicket.comments.some(
-      //   (comment) => comment.commentId === emailId
-      // );
-      // if (!isDuplicateComment) {
-      //   const saveComment = await OutlookMailRepository.Addcomment(
-      //     emailId,
-      //     emailResponse,
-      //     existingTicket
-      //   );
-      //   console.log("Reply added as a comment.", saveComment);
-      //   return {
-      //     success: true,
-      //     message: "Reply added as comment in Database"
-      //   };
-      // } else {
-      //   console.log("Duplicate comment detected, skipping.");
-      // }
-
-      const saveComment = await OutlookMailRepository.Addcomment(
-        emailId,
-        emailResponse,
-        existingTicket
+      const isDuplicateComment = existingTicket.comments.some(
+        (comment) => comment.commentId === emailId
       );
-      console.log("Reply added as a comment.", saveComment);
-      return {
-        success: true,
-        message: "Reply added as comment in Database"
-      };
+      if (!isDuplicateComment) {
+        const saveComment = await OutlookMailRepository.Addcomment(
+          emailId,
+          emailResponse,
+          existingTicket
+        );
+        console.log("Reply added as a comment.", saveComment);
+        return {
+          success: true,
+          message: "Reply added as comment in Database"
+        };
+      } else {
+        console.log("Duplicate comment detected, skipping.");
+      }
     }
     return {
       success: false,
